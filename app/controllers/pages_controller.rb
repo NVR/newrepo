e239b3b0b28dcee61bef1c636287c7c008b5ab14
Anvar Gabsalikhov <envy.94@mail.ru>
2011-09-25 12:59:13 +0400

    titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,17 @@
 class PagesController < ApplicationController
   def home
+  @title="home"
   end
 
   def contact
+  @title="contact"
   end
 
   def event
+  @title="event"
+  end
+  
+  def pages
   end
 
 end
