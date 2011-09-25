bea4fc056b4163f26b9ff69102b78743a0ff61b6
Anvar Gabsalikhov <envy.94@mail.ru>
2011-09-25 19:14:34 +0400

    smth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+  def show
+  @user = User.find(params[:id])
+  @title = @user.name 
+  end
+  
+  
   def new
   @title="Sign up"
   end
-  def show
-  @user = User.find(params[:id])
-  end
+  
 end
