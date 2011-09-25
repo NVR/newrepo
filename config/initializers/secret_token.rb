56492ad20494a5bf1d4704ca4d5625c6a4bddcf9
Anvar Gabsalikhov <envy.94@mail.ru>
2011-09-25 12:11:46 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newrepo::Application.config.secret_token = 'b453f68accd46fa22af8d42365bf47ad67562c4b5fae2a9b1c4d0354b43997fcdc472ec2bc065e13f4434b7b4979cd3ac87390277d16695c034f901ea746ad1e'
