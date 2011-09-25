1efd559c65ba09b5e419333e0db5ef2d62e4e378
Anvar Gabsalikhov <envy.94@mail.ru>
2011-09-25 12:28:21 +0400

    added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Newrepo::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/event"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
